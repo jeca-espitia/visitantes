0cd5160e15627833a513e436ce36af03fe0ba1f1
camila-espitia <camila.espitia@gmail.com>
2019-09-17 18:51:10 -0500

    Commit uno

---
+++ app.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var mongoose = require("mongoose");
+mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true });
+mongoose.connection.on("error", function(e) { console.error(e); });
+
+// definimos el schema
+var schema = mongoose.Schema({
+    nombre: String,
+    date: { type: Date, default: Date.now() },
+   //ObjectId: ,
+    published: { type: Boolean, default: false }
+  });
+  
+  // definimos el modelo
+var visitantes = mongoose.model("visitantes", schema);
+var app = express();
+
+app.get('/', (req, res) => {
+	  if (!req.query.nombre){
+        visitantes.create({ nombre: "anomimo"}, function(err) {   
+            res.send("<h1>Hola desconocido!</h1>");    
+            });
+        
+	  }else{
+            visitantes.create({ nombre: req.query.nombre}, function(err) { 
+                res.send("<h1>El visitante fue almacenado con éxito</h1>");
+        });
+	  } 
+	}); 
+    app.listen(3000, () => console.log('Listening on port 3000!'));
