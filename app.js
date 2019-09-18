297cb4e05460984ba5e6efe08b51ebfed8f1410b
camila-espitia <camila.espitia@gmail.com>
2019-09-17 19:22:55 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,31 @@
 var express = require('express');
 var mongoose = require("mongoose");
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/mongo-1', { useNewUrlParser: true });
-mongoose.connection.on("error", function(e) { console.error(e); });
+var app = express();
+
 
 // definimos el schema
 var schema = mongoose.Schema({
     name: String,
-    date: { type: Date, default: Date.now() }
+    date: { type: Date, default: Date.now(),
+      published: { type: Boolean, default: false } }
   });
   
   // definimos el modelo
-var visitantes = mongoose.model("visitantes", schema);
-var app = express();
+var Visitor = mongoose.model("Visitor", schema);
+
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/mongo-1', { useNewUrlParser: true });
+mongoose.connection.on("error", function(e) { console.error(e); });
 
 app.get('/', (req, res) => {
 	  if (!req.query.name){
-        visitantes.create({ name: "Anónimo"}, function(err) {   
+      Visitor.create({ name: "Anónimo"}, function(err) {   
           res.send("<h1>El visitante fue almacenado con éxito</h1>");
             });
         
 	  }else{
-            visitantes.create({ name: req.query.name}, function(err) { 
+               Visitor.create({ name: req.query.name}, function(err) { 
                 res.send("<h1>El visitante fue almacenado con éxito</h1>");
         });
 	  } 
 	}); 
-    app.listen(3000, () => console.log('Listening on port 3000!'));
+app.listen(3000, () => console.log('Listening on port 3000!'));
